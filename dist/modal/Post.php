7af623cb26383652097de36414e34464c28fcedd
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2020-06-16 07:43:56 +0530

    Update api endpoint created

--- dist/modal/Post.php
+++ dist/modal/Post.php
@@ -110,6 +110,38 @@
             
             return false;
         }
+
+        // Update Post
+        public function updatePost() {
+            // Create Query
+            $query = 'UPDATE ' . $this->table . ' SET title = :title, body = :body, author = :author, category_id = :category_id WHERE id = :id';
+
+            // Prepare Statment
+            $stmt = $this->connection->prepare($query);
+ 
+            // Data Cleaning
+            $this->id = htmlspecialchars(strip_tags($this->id));
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->body = htmlspecialchars(strip_tags($this->body));
+            $this->author = htmlspecialchars(strip_tags($this->author));
+            $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+            // Data Binding
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':body', $this->body);
+            $stmt->bindParam(':author', $this->author);
+            $stmt->bindParam(':category_id', $this->category_id);
+            
+            if($stmt->execute()) {
+                return true;
+            } 
+
+            // Something Went Wrong
+            printf("Error: %s. \n", $stmt->error);
+            
+            return false;
+        }
     }
 
 ?>
